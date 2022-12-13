f4d032a1cb1fd19ee7deac1043895067b6fd48c7
Auguste Baum <auguste.apple@gmail.com>
2022-12-13 14:09:31 +0100

    refactor: Remove dead code.

--- old/experiments/ae_reconstruction/__main__.py
+++
@@ -1,4 +0,0 @@
-from .experiment import ae_reconstruction
-
-if __name__ == "__main__":
-    ae_reconstruction()
--- old/experiments/latent_shift/__main__.py
+++
@@ -1,4 +0,0 @@
-from .experiment import latent_shift
-
-if __name__ == "__main__":
-    latent_shift()
