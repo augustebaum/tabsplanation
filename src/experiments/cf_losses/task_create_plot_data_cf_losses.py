8edb7b9b1d513b4adb4b76fc438acc66d6cce341
Auguste Baum <auguste.apple@gmail.com>
2023-01-18 09:10:29 +0100

    feat: Develop CF losses experiment to produce more things.

--- src/experiments/cf_losses/task_create_plot_data_cf_losses.py
+++ src/experiments/cf_losses/task_create_plot_data_cf_losses.py
@@ -1,16 +1,62 @@
+"""Analyze different loss functions that can be used to make the CFX change the
+predicted class correctly.
+
+1. For an array of inputs `x` covering the input space, show what the loss is.
+2. For an array of latents `z` covering the latent space, show what the loss is
+for the decoded points. Show where the classes have been mapped.
+3. In all cases, show the gradient of the loss in latent space.
+"""
 import pickle
+from typing import Dict, TypedDict, TypeVar
 
 import torch
 
 from config import BLD_PLOT_DATA
 from experiments.shared.task_create_cake_on_sea import TaskCreateCakeOnSea
 from experiments.shared.task_train_model import TaskTrainModel
-from experiments.shared.utils import define_task, get_data_module, setup, Task
+from experiments.shared.utils import define_task, get_data_module, Task
 from tabsplanation.types import Tensor
 
 
-def get_z0(cfg):
-    return torch.linspace(cfg.lo, cfg.hi, steps=cfg.nb_steps)
+def get_z0():
+    return torch.linspace(-5, 5, steps=25)
+
+
+def get_x0():
+    return torch.linspace(-5, 55, steps=25)
+
+
+T = TypeVar("T")
+LossName = str
+ClassNumber = int
+
+ResultDict = Dict[LossName, Dict[ClassNumber, T]]
+
+
+class Gradients(TypedDict):
+    """Input to `numpy.streamplot`."""
+
+    x: Tensor["nb_steps", "nb_steps"]
+    y: Tensor["nb_steps", "nb_steps"]
+    u: Tensor["nb_steps", "nb_steps"]
+    v: Tensor["nb_steps", "nb_steps"]
+
+
+class LatentSpaceMap(TypedDict):
+    z: Tensor["nb_points", "latent_dim"]
+    class_: Tensor["nb_points", int]
+
+
+Loss = Tensor[float]
+
+
+class CfLossesResult(TypedDict):
+
+    x_losses: ResultDict[Loss]
+    z_losses: ResultDict[Loss]
+    latent_space_map: LatentSpaceMap
+    x_gradients: ResultDict[Gradients]
+    z_gradients: ResultDict[Gradients]
 
 
 class TaskCreatePlotDataCfLosses(Task):
@@ -30,47 +76,69 @@ class TaskCreatePlotDataCfLosses(Task):
     @classmethod
     def task_function(cls, depends_on, produces, cfg):
 
-        # device = setup(cfg.seed)
-
-        z0 = get_z0(cfg)
+        z0 = get_z0()
         z: Tensor["nb_points", 2] = torch.cartesian_prod(z0, z0)
 
-        # data_module = get_data_module(depends_on, cfg, device)
-        # dataset = data_module.dataset
+        x0 = get_x0()
+        x: Tensor["nb_points", 2] = torch.cartesian_prod(x0, x0)
 
-        # inputs = dataset.fill_from_2d_point(inputs_x)
-        # normalized_inputs = dataset.normalize(inputs)
+        data_module = get_data_module(depends_on, cfg, "cpu")
+        dataset = data_module.dataset
+
+        inputs = dataset.fill_from_2d_point(x)
+        normalized_inputs = dataset.normalize(inputs)
 
         classifier = torch.load(depends_on["classifier"]["model"])
         autoencoder = torch.load(depends_on["autoencoder"]["model"])
 
+        # TODO read the classes from config
         from tabsplanation.explanations.losses import (
             AwayLoss,
-            BabyStretchOutLoss,
-            StretchOutLoss,
+            BabyStretchLoss,
+            StretchLoss,
         )
 
-        model_logits = classifier(autoencoder.decode(z))
-        source = model_logits.argmax(dim=-1)
+        def compute_losses(model_logits):
+            """Given some model outputs, compute the loss for all choices of target
+            class and loss function.
 
-        # torch doesn't seem to have a more convenient map function
-        # source_to_target = {0: 1, 1: 2, 2: 0}
-        # target = torch.zeros(len(source), dtype=int)
-        # target.map_(source, lambda _, s: source_to_target.get(s))
-
-        nb_classes = 3
-        # result = {"inputs": inputs, "latents": autoencoder.encode(normalized_inputs)}
-        result = {
-            loss.__name__: {
-                class_: loss()(
-                    model_logits,
-                    source=source,
-                    target=torch.full((len(source),), class_),
-                )
-                for class_ in range(nb_classes)
+            For each data points the source class is taken to be the predicted class for
+            the given logit.
+            """
+            nb_classes = 3
+            return {
+                loss.__name__: {
+                    class_: loss()(
+                        model_logits,
+                        source=model_logits.argmax(dim=-1),
+                        target=torch.full((len(model_logits),), class_),
+                    )
+                    for class_ in range(nb_classes)
+                }
+                for loss in [AwayLoss, BabyStretchLoss, StretchLoss]
             }
-            for loss in [AwayLoss, BabyStretchOutLoss, StretchOutLoss]
+
+        result = {}
+        f_x = classifier(normalized_inputs)
+        result["x_losses"] = compute_losses(f_x)
+        # 2.a.
+        result["z_losses"] = compute_losses(classifier(autoencoder.decode(z)))
+        # 2.b.
+        result["latent_space_map"] = {
+            "z": autoencoder.encode(normalized_inputs),
+            "class": f_x.argmax(dim=-1),
         }
+
+        # 3.a. Gradients of the losses, taken with respect to x
+        # result["x_gradients"] =  {
+        #         loss.__name__: {
+        #             class_: torch.autograd.grad(result["x_losses"][loss.__name__][class_], x)[0]
+        #             for class_ in range(nb_classes)
+        #         }
+        #         for loss in [AwayLoss, BabyStretchOutLoss, StretchOutLoss]
+        #     }
+        # 3.a. Gradients of the losses, taken with respect to z, mapped back to the input space
+        # result["z_gradients"] =
 
         with open(produces["results"], "wb") as results_file:
             pickle.dump(result, results_file)
--- src/experiments/cf_losses/task_plot_cf_losses.py
+++ src/experiments/cf_losses/task_plot_cf_losses.py
@@ -6,10 +6,13 @@ from matplotlib.colors import LinearSegmentedColormap
 
 from config import BLD_PLOTS
 from experiments.cf_losses.task_create_plot_data_cf_losses import (
+    get_x0,
     get_z0,
+    Loss,
+    ResultDict,
     TaskCreatePlotDataCfLosses,
 )
-from experiments.shared.utils import define_task, load_mpl_style, Task
+from experiments.shared.utils import define_task, get_map_img, load_mpl_style, Task
 
 
 class TaskPlotCfLosses(Task):
@@ -21,22 +24,70 @@ class TaskPlotCfLosses(Task):
 
         self.depends_on = TaskCreatePlotDataCfLosses(cfg).produces
 
-        self.produces |= {"plot": self.produces_dir / "plot.svg"}
+        self.produces |= {
+            "x_losses": self.produces_dir / "x_losses.svg",
+            "z_losses": self.produces_dir / "z_losses.svg",
+            "latent_space_map": self.produces_dir / "latent_space_map.svg",
+        }
 
     @classmethod
     def task_function(cls, depends_on, produces, cfg):
+
         with open(depends_on["results"], "rb") as results_file:
             results = pickle.load(results_file)
 
-        z0 = get_z0(cfg)
-        z0, z1 = torch.meshgrid(z0, z0)
-        nb_classes = 3
-
         load_mpl_style()
-        fig, axes = plt.subplots(
-            nrows=nb_classes, ncols=len(results), layout="constrained"
+
+        # 1. Losses in input space
+        fig = TaskPlotCfLosses.plot_all_losses_and_targets(
+            results["x_losses"],
+            x_axis=get_x0(),
+            axis_limits=[-5, 55, -5, 55],
+            axis_labels=(r"$x_0$", r"$x_1$"),
+        )
+        # ax.imshow(get_map_img(), origin="upper", extent=[0, 50, 0, 50])
+
+        fig.savefig(produces["x_losses"])
+
+        # 2. Losses in latent space
+        fig = TaskPlotCfLosses.plot_all_losses_and_targets(
+            results["z_losses"],
+            x_axis=get_z0(),
+            axis_limits=[-5, 5, -5, 5],
+            axis_labels=(r"$z_0$", r"$z_1$"),
         )
 
+        fig.savefig(produces["z_losses"])
+
+        # 2.b. Class distribution in latent space
+        z = results["latent_space_map"]["z"].detach()
+
+        fig, ax = plt.subplots()
+        ax.scatter(
+            z[:, 0],
+            z[:, 1],
+            c=results["latent_space_map"]["class"].detach(),
+            alpha=0.5,
+            marker="s",
+            zorder=1,
+        )
+
+        fig.savefig(produces["latent_space_map"])
+
+        # plt.show(block=True)
+
+    @staticmethod
+    def plot_all_losses_and_targets(
+        results: ResultDict[Loss], x_axis, axis_limits, axis_labels
+    ):
+        x0, x1 = torch.meshgrid(x_axis, x_axis)
+        nb_classes = len(results[list(results.keys())[0]])
+
+        fig, axes = plt.subplots(
+            nrows=nb_classes, ncols=len(results), layout="constrained", figsize=(12, 10)
+        )
+
+        # Row labels (class number)
         for class_ in range(nb_classes):
             axes[class_, 0].annotate(
                 f"Target class is {class_}",
@@ -49,25 +100,29 @@ class TaskPlotCfLosses(Task):
                 va="center",
             )
 
-        for i, (loss_name, loss_data) in enumerate(results.items()):
-            for class_ in range(nb_classes):
-                ax = axes[class_][i]
+        for col, (loss_name, loss_data) in enumerate(results.items()):
+            for row, class_ in enumerate(range(nb_classes)):
+
+                ax = axes[row][col]
+
                 cs = ax.contourf(
-                    z0,
-                    z1,
-                    loss_data[class_].detach().reshape((cfg.nb_steps, cfg.nb_steps)),
+                    x0,
+                    x1,
+                    loss_data[class_].detach().reshape((len(x0), len(x0))),
                     zorder=1,
-                    cmap=LinearSegmentedColormap.from_list("", ["white", "red"], N=256),
+                    cmap=LinearSegmentedColormap.from_list("", ["white", "red"]),
                     norm=plt.Normalize(),
                 )
 
-                ax.axis([cfg.lo, cfg.hi, cfg.lo, cfg.hi])
-                ax.set_title(loss_name)
-                ax.set_xlabel(r"$z_0$")
-                ax.set_ylabel(r"$z_1$")
+                ax.axis(axis_limits)
+                ax.set_xlabel(axis_labels[0])
+                ax.set_ylabel(axis_labels[1])
+                if row == 0:
+                    ax.set_title(loss_name)
 
-        fig.colorbar(cs)
-        plt.show(block=True)
+            fig.colorbar(cs, ax=ax, location="bottom")
+
+        return fig
 
 
 task, task_definition = define_task("cf_losses", TaskPlotCfLosses)
