2d44ba701f95f2f08f02856be0bf15642f2e74b6
Auguste Baum <auguste.apple@gmail.com>
2023-01-25 16:59:27 +0100

    dura auto-backup

--- src/experiments/path_regularization/task_plot_path_reg.py
+++ src/experiments/path_regularization/task_plot_path_reg.py
@@ -35,9 +35,10 @@ class TaskPlotPathReg(Task):
         # Plot latent space maps
         fig, ax = plt.subplots(nrows=1, ncols=2)
 
-        TaskPlotCfLosses.plot_latent_space_map(ax[0,0], results[""unregularized_latent_space_map""])
+        TaskPlotCfLosses.plot_latent_space_map(ax[0,0], results["unregularized_latent_space_map"])
+        TaskPlotCfLosses.plot_latent_space_map(ax[0,1], results["path_regularized_latent_space_map"])
             
-            "path_regularized_latent_space_map"
+            
 
 
 
