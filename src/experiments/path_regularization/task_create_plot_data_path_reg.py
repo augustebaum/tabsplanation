dab17d7152811563fab95e94943d7da0aec40ff2
Auguste Baum <auguste.apple@gmail.com>
2023-01-25 16:32:49 +0100

    dura auto-backup

--- src/experiments/path_regularization/task_create_plot_data_path_reg.py
+++ src/experiments/path_regularization/task_create_plot_data_path_reg.py
@@ -47,6 +47,16 @@ class TaskCreatePlotDataPathRegularization(Task):
 
         results = {}
 
+        x0 = get_x0()
+        x: Tensor["nb_points", 2] = torch.cartesian_prod(x0, x0)
+
+        data_module = get_data_module(depends_on, cfg, "cpu")
+        dataset = data_module.dataset
+
+        inputs = dataset.fill_from_2d_point(x)
+        normalized_inputs = dataset.normalize(inputs)
+        normalized_inputs.requires_grad = True
+
         # 1. Plot the latent spaces
         results |= {
             "unregularized_latent_space_map": TaskCreatePlotDataCfLosses.latent_space_map(
