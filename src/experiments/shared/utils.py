a86db7d9fabd6ab291980bed7aa9ab7294f9bbc0
Auguste Baum <auguste.apple@gmail.com>
2023-01-25 16:49:02 +0100

    dura auto-backup

--- src/experiments/shared/utils.py
+++ src/experiments/shared/utils.py
@@ -190,7 +190,7 @@ def read_pkl(file_path):
     return result
 
 
-def read(file_path: Path) -> Any:
+def read(file_path: Path) -> Object:
     read_variants = {"pkl": read_pkl}
 
     read_fn = read_variants.get(file_path.suffix)
