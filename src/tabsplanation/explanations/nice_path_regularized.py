d60a84a10fb33a3afcb73f016c61753fd6c6544f
Auguste Baum <auguste.apple@gmail.com>
2023-01-25 10:26:14 +0100

    dura auto-backup

--- src/tabsplanation/explanations/nice_path_regularized.py
+++ src/tabsplanation/explanations/nice_path_regularized.py
@@ -89,7 +89,7 @@ class BoundaryCrossLoss(nn.Module):
         prbs: Tensor["batch * nb_steps", "nb_classes"] = classifier.predict_proba(
             inputs
         )
-        prbs = prbs.reshape(latents.shape)
+        prbs = prbs.reshape(latents.shape[0], latents.shape[0])
         import pdb
 
         pdb.set_trace
