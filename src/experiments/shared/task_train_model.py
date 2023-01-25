7489f8e2bd1c9e4d55dfda883ae8f651cc9ddb0e
Auguste Baum <auguste.apple@gmail.com>
2023-01-25 14:53:12 +0100

    dura auto-backup

--- src/experiments/shared/task_train_model.py
+++ src/experiments/shared/task_train_model.py
@@ -75,7 +75,7 @@ class TaskTrainModel:
             logger=tb_logger,
             callbacks=[early_stopping_cb],
             enable_model_summary=False,
-            val_check_interval=0.25,
+            val_check_interval=cfg.training.val_check_interval,
         )
 
         trainer.fit(model=model, datamodule=data_module)
