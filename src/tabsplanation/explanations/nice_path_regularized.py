fix: Correct validity losses computation.
- Vectorize validity rate computation for better performance.
- Fix off-by-one bug in generation of random targets.

src/tabsplanation/explanations/nice_path_regularized.py
@@ -11,7 +11,7 @@ Explainer = Any
 
 
 def random_targets_like(y: Tensor, nb_classes: int):
-    batch_target_difference = torch.randint_like(y, low=1, high=nb_classes - 1)
+    batch_target_difference = torch.randint_like(y, low=1, high=nb_classes)
     batch_target_class = (y + batch_target_difference) % nb_classes
     return batch_target_class
 